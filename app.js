fae8c7567a7af4700c7a3ef3f4b6602df812ba0b
anudaweerasinghe <anuda@live.com>
2021-03-04 00:32:10 -0500

    added cors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyPasser.json());
 
 app.use((req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Token');
     if(req.method === 'OPTIONS'){
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
